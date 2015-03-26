2a6f3381cfc97d70d4ab5bd5f254b8d0444abfe2
David Regatos <david.regatos@gmail.com>
2015-03-26 12:20:58 +0100

    Created Library model (DRGLibrary);

---
+++ MyHackerBooks_v2/Models/DRGLibrary.h
@@ -0,0 +1,46 @@
+//
+//  DRGLibrary.h
+//  MyHackerBooks_v2
+//
+//  Created by David Regatos on 26/03/15.
+//  Copyright (c) 2015 DRG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class DRGBook;
+
+@interface DRGLibrary : NSObject
+
+/**
+ Returns a full list of bookd ORDERED alphabetically
+ */
+- (NSArray *)bookList;
+
+/**
+ Returns the number of books in the library
+ */
+- (NSUInteger)booksCount;
+
+/**
+ Returns an ordered list (alphabetically) of the full tag list
+ */
+- (NSArray *)tags;
+
+/**
+ Returns the number of books for a given 'tag'
+ */
+- (NSUInteger)bookCountForTag:(NSString *)tag;
+
+/**
+ Returns an ordered list (alphabetically) of books for a given 'tag'
+ */
+- (NSArray *)booksForTag:(NSString *)tag;
+
+/**
+ Returns the book at given index of the ordered list (alphabetically)
+ of books for a given 'tag'
+ */
+- (DRGBook *)bookForTag:(NSString *)tag atIndex:(NSUInteger)index;
+
+@end
---
+++ MyHackerBooks_v2/Models/DRGLibrary.m
@@ -0,0 +1,14 @@
+//
+//  DRGLibrary.m
+//  MyHackerBooks_v2
+//
+//  Created by David Regatos on 26/03/15.
+//  Copyright (c) 2015 DRG. All rights reserved.
+//
+
+#import "DRGLibrary.h"
+#import "DRGBook.h"
+
+@implementation DRGLibrary
+
+@end
